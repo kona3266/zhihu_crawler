b06ff79f95c926ad78b13b246e97b055fd60c2e4
kona3266 <a66b01c06@gmail.com>
2020-11-28 21:54:29 +0800

    remove sql template

--- user_list.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `zhihu` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) COLLATE utf8_bin NOT NULL,
-    `follower` int(11) COLLATE utf8_bin NOT NULL,
-	`edu` varchar(255) COLLATE utf8_bin NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-AUTO_INCREMENT=1 ;
